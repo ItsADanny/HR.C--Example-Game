c8e52be51e8341a70f69a606d280bf324d6bd5ac
ItsDanny <danny.desnoo@ddesnoo.nl>
2024-09-10 00:33:28 +0200

    Added some O assignments
    Added some of the O assignments,
    not all of them are working according to how codegrade wants it so i am re-writing them...
    
    These will follow in a later commit...

---
+++ Example Console Game/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml.Serialization;
+
+Int32 Player_pos_x = 0, Player_pos_y = 0;
+Int32 Max_pos_x_plus = 10, Max_pos_y_plus = 10, Max_pos_x_minus = -10, Max_pos_y_minus = -10;
+
+
+
+
+
+
+
+
+
+
+
+void print_movement()
+{
+    throw new NotImplementedException();
+}
+
+void print_board()
+{
+    throw new NotImplementedException();
+}
+
+while (true) {
+    print_board();
+    print_movement();
+
+    player_choice = Console.ReadLine();
+
+    switch (player_choice)
+    {
+        case "up":
+
+            break;
+        case "down":
+        
+            break;
+        default:
+    }
+}
